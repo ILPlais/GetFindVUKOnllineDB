2fdf72dd33b4d552b7c4516e0caba5080492b379
PLAIS Lionel <lionel.plais@ilp-web.net>
2023-10-27 11:39:35 +0200

    Adding verifications
    Adding verifications for:
    — The AACS library, with automatic installation in the VLC directory on Windows if administration rights.
    — Check if Java is installed under Windows.
    — Check the online database last update to know if it is newer than the local database.
    — Use a temporary file during the downloads.

--- GetFindVUKOnllineDB.ps1
+++ GetFindVUKOnllineDB.ps1
@@ -6,42 +6,119 @@ if (-not ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdenti
 else {
 	# The user is an administrator: use the global directory
 	$target = "$env:ALLUSERSPROFILE\aacs"
+
+	# Verify if VLC is install
+	$vlcPath = "$env:PROGRAMFILES\VideoLAN\VLC"
+
+	if (Test-Path -Path $vlcPath) {
+		# Verify if there is the AACS DLL in the VLC directory
+		$vlcDllPath = "$vlcPath\libaacs.dll"
+
+		Write-Host "[*] Verify if the libaacs DLL is in the VLC directory…"
+		if (-not (Test-Path -Path $vlcDllPath)) {
+			Write-Host "[!] Library AACS is missing!"
+			# Check if Windows is in 32 bits or in 64 bits
+			if ([Environment]::Is64BitOperatingSystem) {
+				Write-Host "[+] Downloading the 32 bits version…"
+				$libaacsUrl = "https://vlc-bluray.whoknowsmy.name/files/win64/libaacs.dll"
+			} else {
+				Write-Host "[+] Downloading the 64 bits version…"
+				$libaacsUrl = "https://vlc-bluray.whoknowsmy.name/files/win32/libaacs.dll"
+			}
+
+			# Download the AACS DLL in the VLC directory
+			Invoke-WebRequest -Uri $libaacsUrl -OutFile $vlcDllPath
+		}
+	}
+}
+
+# Check if Java is installed, for the Blu-ray menus
+try {
+	$javaVersion = & java -version 2>&1
+	Write-Host "[*] Java is installed.`n`t-> Version: $javaVersion."
+}
+catch {
+	Write-Host "[!] Java seem to not be installed. You will need to install it to show the Blu-ray menus."
 }
 
 # If the target directory is missing, create it
 if (-not (Test-Path -Path $target -PathType Container)) {
-	Write-Host "Directory ""$target"" is missing!"
-	Write-Host "Creating ""$target""…"
+	Write-Host "[!] Directory ""$target"" is missing!"
+	Write-Host "[+] Creating ""$target""…"
 	$null = New-Item -Path $target -ItemType Directory -ErrorAction Stop
 }
+
+Write-Host "[*] We will use the directory:`n`t-> $target"
+
+# Set the database Web site URL
+$databaseWebSite = "http://fvonline-db.bplaced.net/"
+
+# Get the database last update
+Write-Host "[+] Get the database last update on the website:`n`t-> $databaseWebSite"
+$pageContent = (Invoke-WebRequest -Uri $databaseWebSite).Content
+
+if ($pageContent -match "LastUpdate:\s*(\d{4}-\d{2}-\d{2}\s*\d{2}:\d{2}:\d{2})") {
+	$lastUpdate = [DateTime]::ParseExact($Matches[1], "yyyy-MM-dd HH:mm:ss", $null)
+	Write-Host "[*] The last update of the Web database is $($lastUpdate.ToString('yyyy-MM-dd HH:mm:ss'))."
+}
 else {
-	# Delete the existing KEYDB.cfg file
-	Remove-Item -Path "$target\KEYDB.cfg" -ErrorAction SilentlyContinue
+	$lastUpdate = [DateTime]::ParseExact("1970-01-01", "yyyy-MM-dd", $null)
 }
 
-# Get the list of zip file links from the website
-$links = (Invoke-WebRequest -UseBasicParsing -Uri "http://fvonline-db.bplaced.net/").Links | Where-Object { $_.href -like "http://*/fv_download.php?lang=*" } | Select-Object -ExpandProperty href
-
-# Create a temporary directory
-$tempdir = Join-Path $env:TEMP $(New-Guid)
-New-Item -Type Directory -Path $tempdir | Out-Null
-
-# For each link
-foreach ($link in $links) {
-	# Download the zip file
-	Write-Host "Downloading the file: ""$link""…"
-	Invoke-WebRequest -UseBasicParsing -Uri $link -OutFile "$tempdir\keydb.zip"
-
-	# Unzip the file
-	Write-Host "Unzip the file: ""$tempdir\keydb.zip""…"
-	Expand-Archive -Path "$tempdir\keydb.zip" -DestinationPath $tempdir
-
-	# Add the contents of the keydb.cfg file to the KEYDB.cfg
-	Add-Content -Path "$target\KEYDB.cfg" -Value (Get-Content -Path "$tempdir\keydb.cfg")
-
-	# Remove the downloaded and extracted files
-	Remove-Item -Path "$tempdir\*" -Force
+# Get the local database last update
+if (Test-Path -Path "$target\lastupdate.txt") {
+	$fileContent = Get-Content -Path "$target\lastupdate.txt"
+	$localUpdate = [DateTime]::ParseExact($fileContent, "yyyy-MM-dd HH:mm:ss", $null)
+	Write-Host "[*] The last update of the local database is $($localUpdate.ToString('yyyy-MM-dd HH:mm:ss'))."
+}
+else {
+	$localUpdate = [DateTime]::ParseExact("1970-01-01", "yyyy-MM-dd", $null)
 }
 
-# Remove the temporary directory
-Remove-Item -Path $tempdir -Force
+# Check if the database on the website is newer than the local database
+if ($lastUpdate -gt $localUpdate) {
+	# Get the list of zip file links from the website
+	Write-Host "[+] Get the list of zip file links from the website…"
+	$links = (Invoke-WebRequest -UseBasicParsing -Uri $databaseWebSite).Links | Where-Object { $_.href -like "http://*/fv_download.php?lang=*" } | Select-Object -ExpandProperty href
+
+	# Create a temporary directory
+	$tempdir = Join-Path $env:TEMP $(New-Guid)
+	New-Item -Type Directory -Path $tempdir | Out-Null
+
+	# For each link
+	foreach ($link in $links) {
+		# Download the zip file
+		Write-Host "[+] Downloading the file:`n`t-> ""$link""…"
+		Invoke-WebRequest -UseBasicParsing -Uri $link -OutFile "$tempdir\keydb.zip"
+
+		# Unzip the file
+		Write-Host "[+] Unzip the file:`n`t-> ""$tempdir\keydb.zip""…"
+		Expand-Archive -Path "$tempdir\keydb.zip" -DestinationPath $tempdir
+
+		# Add the contents of the keydb.cfg file to the KEYDB.cfg
+		Write-Host "[+] Add the content of the KEYDB file to the local KEYDB.cfg…"
+		Add-Content -Path "$target\KEYDB.cfg.tmp" -Value (Get-Content -Path "$tempdir\keydb.cfg")
+
+		# Remove the downloaded and extracted files
+		Remove-Item -Path "$tempdir\*" -Force
+	}
+
+	# Remove the temporary directory
+	Remove-Item -Path $tempdir -Force
+
+	# Delete the actual KEYDB.cfg
+	if (Test-Path -Path "$target\KEYDB.cfg") {
+		Write-Host "[+] Delete the actual KEYDB.cfg…"
+		Remove-Item -Path "$target\KEYDB.cfg"
+	}
+
+	# Rename the temporary KEYDB.cfg
+	Write-Host "[+] Rename the temporary KEYDB.cfg file…"
+	Rename-Item -Path "$target\KEYDB.cfg.tmp" -NewName "$target\KEYDB.cfg"
+
+	# Save the database last update into the "lastupdate.txt" file
+	$lastUpdateFormatted = $lastUpdate.ToString("yyyy-MM-dd HH:mm:ss")
+	Set-Content -Path "$target\lastupdate.txt" -Value $lastUpdateFormatted
+}
+
+Write-Host "[*] All is done!"
--- GetFindVUKOnllineDB.sh
+++ GetFindVUKOnllineDB.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+# Verify if the AACS library is install
+if ! ldconfig --print-cache | grep --quiet libaacs
+then
+	echo "[!] The library libaacs seem to not be install on your system."
+	echo -e "\t-> Check with your package manager to install it."
+fi
+
 # Set the target directory
 if [ "$(id -u)" -ne 0 ]
 then
@@ -13,34 +20,77 @@ fi
 # If the target directory is missing, create it
 if ! [ -d "$target" ]
 then
-	echo "Directory \"${target}\" is missing!"
-	echo "Creating \"${target}\"…"
+	echo "[!] Directory \"${target}\" is missing!"
+	echo "[+] Creating \"${target}\"…"
 	mkdir -p "$target" || exit 1
-else
-	# Delete the existing KEYDB.cfg file
-	rm -f $target/KEYDB.cfg
 fi
 
-# Get the list of zip file links from the website
-links=$(wget -qO- http://fvonline-db.bplaced.net/ | grep -o 'http://[^"]*fv_download.php?lang=[^"]*')
+echo -e "[*] We will use the directory:\n\t-> $target"
 
-# Create a temporary directory
-tempdir=$(mktemp -d)
+# Set the database Web site URL
+databaseWebSite="http://fvonline-db.bplaced.net/"
 
-# For each link
-for link in $links; do
-	# Download the zip file
-	wget --output-document=$tempdir/keydb.zip $link
+# Get the database last update
+echo -e "[+] Get the database last update on the website:\n\t-> $databaseWebSite"
+pageContent=$(wget -qO- $databaseWebSite)
 
-	# Unzip the file
-	unzip $tempdir/keydb.zip -d $tempdir/
+lastUpdate=$(echo "$pageContent" | grep -o 'LastUpdate: [^<]*' | awk -F': ' '{print $2}')
 
-	# Add the contents of the keydb.cfg file to the KEYDB.cfg
-	cat $tempdir/keydb.cfg >> $HOME/.config/aacs/KEYDB.cfg
+echo "[*] The last update of the Web database is $lastUpdate."
 
-	# Remove the downloaded and extracted files
-	rm -f $tempdir/*
-done
+# Get the local database last update
+if [ -f "$target/lastupdate.txt" ]
+then
+	localUpdate=$(cat "$target/lastupdate.txt")
+	echo "[*] The last update of the local database is $localUpdate."
+else
+	localUpdate="1970-01-01"
+fi
 
-# Remove the temporary directory
-rmdir $tempdir
+# Convert the dates in Unix timestamp
+lastUpdateUnix=$(date --date="$lastUpdate" +%s)
+localUpdateUnix=$(date --date="$localUpdate" +%s)
+
+# Check if the database on the website is newer than the local database
+if [ $lastUpdateUnix -gt $localUpdateUnix ]
+then
+	# Get the list of zip file links from the website
+	echo "[+] Get the list of zip file links from the website…"
+	links=$(wget -qO- $databaseWebSite | grep -o 'http://[^"]*fv_download.php?lang=[^"]*')
+
+	# Create a temporary directory
+	tempdir=$(mktemp -d)
+
+	# For each link
+	for link in $links; do
+		# Download the zip file
+		echo -e "[+] Downloading the file\n\t-> ""$link""…"
+		wget --output-document="$tempdir/keydb.zip" $link
+
+		# Unzip the file
+		echo -e "[+] Unzip the file\n\t-> ""$tempdir/keydb.zip""…"
+		unzip -o "$tempdir/keydb.zip" -d "$tempdir/"
+
+		# Add the contents of the keydb.cfg file to the KEYDB.cfg
+		echo "[+] Add the content of the KEYDB file to the local KEYDB.cfg…"
+		cat "$tempdir/keydb.cfg" >> "$target/KEYDB.cfg.tmp"
+
+		# Remove the downloaded and extracted files
+		rm -f "$tempdir/*"
+	done
+
+	# Remove the temporary directory
+	rmdir -rf "$tempdir"
+
+	# Delete the actual KEYDB.cfg
+	if [ -f "$target/KEYDB.cfg" ]
+	then
+		echo "[+] Delete the actual KEYDB.cfg…"
+	fi
+
+	# Rename the temporary KEYDB.cfg
+	echo "[+] Rename the temporary KEYDB.cfg file…"
+	mv "$target/KEYDB.cfg.tmp" "$target/KEYDB.cfg"
+fi
+
+echo "[*] All is done!"
